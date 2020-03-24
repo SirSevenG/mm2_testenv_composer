eba826918f36c0ec3f7d9600931f7a0650e7fdb8
SirSevenG <ottonseven@gmail.com>
2020-03-24 22:38:58 +0700

    fix start scripts

--- komodo/docker/connect.sh
+++ komodo/docker/connect.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ ${GEN} = "True" ];
   then
-    ./komodod -ac_name=${CHAIN} -ac_cc=${CC} -ac_supply=${SUPPLY} --addnode=${NODE} -gen -server -rest -rpcbind=0.0.0.0  -rpcbind=[::] -rpcallowip=172.0.0.0/8
+    komodod -ac_name=${CHAIN} -ac_cc=${CC} -ac_supply=${SUPPLY} --addnode=${NODE} -gen -server -rest -rpcbind=0.0.0.0  -rpcbind=[::] -rpcallowip=172.0.0.0/8
   else
-  	./komodod -ac_name=${CHAIN} -ac_cc=${CC} -ac_supply=${SUPPLY} --addnode=${NODE} -server -rest -rpcbind=0.0.0.0 -rpcbind=[::] -rpcallowip=172.0.0.0/8
+  	komodod -ac_name=${CHAIN} -ac_cc=${CC} -ac_supply=${SUPPLY} --addnode=${NODE} -server -rest -rpcbind=0.0.0.0 -rpcbind=[::] -rpcallowip=172.0.0.0/8
 fi
