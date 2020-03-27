45c65a9016472c3bbcf990dd9c529512bbd3a38e
SirSevenG <ottonseven@gmail.com>
2020-03-27 14:49:28 +0700

    debug

--- komodo/docker/connect.sh
+++ komodo/docker/connect.sh
@@ -5,7 +5,7 @@ echo $AC
 echo $GEN
 if [ "${GEN}" = "True" ];
   then
-    komodod "${AC}" -gen -addnode="${NODE}"
+    komodod "${AC}" -gen -addnode="${NODE}" -ac_name=WSG -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=172.0.0.0/8
   else
-  	komodod "${AC}"  -addnode="${NODE}"
+  	komodod "${AC}"  -addnode="${NODE}" -ac_name=WSG -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=172.0.0.0/8
 fi
