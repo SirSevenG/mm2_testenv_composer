620ce4f7627447a36efa19b31f034f2c6d7a918f
SirSevenG <ottonseven@gmail.com>
2020-04-06 01:07:50 +0700

    log failed event in swaps test

--- workspace/docker/testlib/test_utils.py
+++ workspace/docker/testlib/test_utils.py
@@ -151,7 +151,7 @@ def check_swap_status(swaps_dict: dict, node_proxy: MMProxy) -> dict:
                 event_occur.append(event_type)
                 if event_type in error_events:
                     print("swap failed uuid: " + str(uuid))
-                    swaps_dict.update({uuid: 'failed'})
+                    swaps_dict.update({uuid: ('failed, event: ' + event_type)})
                     break
                 elif event_type == 'Finished':
                     print("swap success uuid: " + str(uuid))
