8b0550f8ce5f1f96f1c2797c72a9c7dad0d2a581
SirSevenG <ottonseven@gmail.com>
2020-04-05 21:42:46 +0700

    update test start script and .env

--- test_run.sh
+++ test_run.sh
@@ -7,6 +7,6 @@ unzip WSG.zip
 cd ../../.. || return
 chown -R 3003:3003 log
 chown -R 3003:3003 komodo/docker/bootstrap
-cp example-saturation.yml docker-compose.yml
+cp example-"$1".yml docker-compose.yml
 cp example.env .env
 docker-compose up --build
