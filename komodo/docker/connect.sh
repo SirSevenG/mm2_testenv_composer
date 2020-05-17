4e6a81ab29cdee6c461f50bea888967c5d7ab61b
SirSevenG <ottonseven@gmail.com>
2020-05-17 07:45:11 +0700

    allowip

--- komodo/docker/connect.sh
+++ komodo/docker/connect.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 if [ "${GEN}" = "True" ];
   then
-    komodod -ac_name="${AC}" -gen -addnode="${NODE}" -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=172.0.0.0/8
+    komodod -ac_name="${AC}" -gen -addnode="${NODE}" -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0
   else
-  	komodod -ac_name="${AC}"  -addnode="${NODE}" -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=172.0.0.0/8
+  	komodod -ac_name="${AC}"  -addnode="${NODE}" -ac_cc=2 -ac_supply=100000 -ac_blocktime=45 -rpcbind=0.0.0.0 -rpcallowip=0.0.0.0/0
 fi
